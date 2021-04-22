ae5ab5d01cfc336a8229d2d5016a02be99eaafd8
Zack Weber <weberz3@ferris.edu>
2021-04-22 19:03:44 -0400

    Working through creating the Question & Answer pairs.
    Next is setting the correct answer object in the list.

--- AvasureQuizApp/AvasureQuizApp/Program.cs
+++ AvasureQuizApp/AvasureQuizApp/Program.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using AvasureQuizApp.DataModels;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace AvasureQuizApp
 {
@@ -13,13 +17,101 @@ namespace AvasureQuizApp
 
         private static void StartQuiz()
         {
-            //Read in the .txt file, build out question & pair pairs.
+            //Read in the .txt file, build out question & answer pairs.
+            List<Question> questions = ReadQuestionsFile(4);
+
             //Loop through the questions, record responses.
+            PresentQuestion(questions);
+
             //Present an overall score when the last question has been answered.
+            DisplayScore();
+        }
+
+        private static void DisplayScore()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void PresentQuestion(List<Question> questions)
+        {
             throw new NotImplementedException();
         }
 
         //TODO: Error handling
         //TODO: Unit tests
+
+        public static List<Question> ReadQuestionsFile(int possibleAnswers)
+        {
+            List<Question> questions = new List<Question>();
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Data\QuizQuestions.txt");
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                String line;
+                bool foundQuestionLine;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    //Check for the Question text
+                    if (line.StartsWith('('))
+                    {
+                        foundQuestionLine = true;
+                        Question newFoundQuestion = new Question();
+                        newFoundQuestion.Answers = new List<Answer>();
+                        string[] splitLine;
+
+                        //Split on the closed parthesis character.
+                        splitLine = line.Split(')');
+
+                        //Set the question number.
+                        int questionNumber;
+
+                        //Remove the open parthesis character.
+                        splitLine[0] = splitLine[0].Remove(0, 1);
+                        int.TryParse(splitLine[0], out questionNumber);
+                        newFoundQuestion.QuestionNumber = questionNumber;
+
+                        //Set the question text.
+                        newFoundQuestion.QuestionText = splitLine[1].TrimStart();
+
+                        //Read the next (possibleAnswers) lines - 4 responses & correct answer for this exercise, but can support other values.
+                        for (int i = 0; i < possibleAnswers; i++)
+                        {
+                            if (i == possibleAnswers)
+                            {
+                                //Correct answer line
+                                int correctAnswer;
+                                int.TryParse(line, out correctAnswer);
+
+                                //Set the correctAnswer flag on the correct answer object.
+                                Answer correctAnswerObj = newFoundQuestion.Answers.Where(x => x.AnswerNumber == correctAnswer).FirstOrDefault();
+                                correctAnswerObj.IsCorrectAnswer = true;
+
+                                //TODO: Update the item in the List.
+                            }
+                            else
+                            {
+                                //Possible response line
+                                line = sr.ReadLine();
+                                splitLine = line.Split('.');
+
+                                Answer newAnswer = new Answer();
+
+                                int responseNumber;
+                                int.TryParse(splitLine[0], out responseNumber);
+                                newAnswer.AnswerNumber = responseNumber;
+                                newAnswer.AnswerText = splitLine[1].TrimStart();
+
+                                newFoundQuestion.Answers.Add(newAnswer);
+                            }
+                        }
+
+                        questions.Add(newFoundQuestion);
+                    }
+                }
+            }
+
+            return questions;
+        }
     }
 }
